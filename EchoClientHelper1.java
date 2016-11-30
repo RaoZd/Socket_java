7c6f56621cee1ed1278f6256192d650fc0c4a8a0
rzd716 <18813298636@163.com>
2016-11-30 19:51:50 +0700

    基于三层软件的 Echo C/S应用，面向无连接，

---
+++ EchoClientHelper1.java
@@ -0,0 +1,27 @@
+import java.net.*;
+import java.io.*;
+
+public class EchoClientHelper1 {
+   private MyClientDatagramSocket mySocket;
+   private InetAddress serverHost;
+   private int serverPort;
+   EchoClientHelper1(String hostName, String portNum) 
+      throws SocketException, UnknownHostException { 
+  	   this.serverHost = InetAddress.getByName(hostName);
+  		this.serverPort = Integer.parseInt(portNum);
+      // instantiates a datagram socket for both sending
+      // and receiving data
+   	this.mySocket = new MyClientDatagramSocket(); 
+   } 
+   public String getEcho( String message) 
+      throws SocketException, IOException {                                                                                 
+      String echo = "";    
+      mySocket.sendMessage( serverHost, serverPort, message);
+	   // now receive the echo
+      echo = mySocket.receiveMessage();
+      return echo;
+   } //end getEcho
+   public void done( ) throws SocketException {
+      mySocket.close( );
+   }  //end done
+} //end class
